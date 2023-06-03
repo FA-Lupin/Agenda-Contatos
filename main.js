d66464aa408a2da2edccde9ff3239292151db008
Felipe Augusto <workcontact.dev@gmail.com>
2023-06-02 22:01:18 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const form = document.getElementById("formctt");
+
+const Nomes = [];
+const Numeros = [];
+
+let linhas = "";
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const inputNome = document.getElementById("Nome");
+  const inputNumero = document.getElementById("Numero");
+
+  if (numeros.includes(parseFloat(inputNumero.value))) {
+    alert(`O número ${inputNumero.value} já está na lista.`);
+  } else {
+    nomes.push(inputNome.value);
+    numeros.push(parseFloat(inputNumero.value));
+  }
+
+  let linha = "<tr>";
+  linha += "<td>" + inputNome.value + "</td>";
+  linha += "<td>" + inputNumero.value + "</td>";
+  linha += "</tr>";
+
+  linhas += linha;
+
+  const corpoTabela = document.querySelector("tbody");
+  corpoTabela.innerHTML = linhas;
+
+  inputNome.value = "";
+  inputNumero.value = "";
+});
